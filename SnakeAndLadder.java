955abcc3a37a7f5ee24d408303ccba2cc20d79dc
akashkbarik <akashkbarik12@gmail.com>
2023-02-25 01:05:59 +0530

    Refactored: random dice number code

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,5 +3,7 @@ package Day4.asssignments.SnakeAndLadder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int POSITION = 0;
+        int dice_number = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println(dice_number);
     }
 }
