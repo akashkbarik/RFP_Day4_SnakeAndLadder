27a024d5d936614f3b6e10b00ec2a96b9114fceb
akashkbarik <akashkbarik12@gmail.com>
2023-02-25 01:23:26 +0530

    Refactored: two player mode code added.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,8 @@
 package Day4.asssignments.SnakeAndLadder;
 
 public class SnakeAndLadder {
-    static int PLAYER_POSITION = 0;
+    static int PLAYER1_POSITION = 0;
+    static int PLAYER2_POSITION = 0;
     static int count;
 
     static int random_value(int max) {
@@ -22,13 +23,21 @@ public class SnakeAndLadder {
     static int Calculate_ladder(int dice, int player) {
         System.out.println("ladder = " + dice);
         int temp = player + dice;
-        if (temp <= 100)
+//        play again
+        System.out.println("its a ladder! player will play again");
+        System.out.println("ladder = " + dice);
+        int temp2 = player + dice;
+        player = temp2;
+        if (temp2 <= 100) {
             player = temp;
-        else if (temp > 100) {
-            System.out.println("oops! its " + temp + " you cant go ahead. 100 is your dead end .");
+        } else if (temp2 > 100) {
+            player=temp2-dice;
+            System.out.println("oops! its " + temp2 + " you cant go ahead. 100 is your dead end .");
         }
         return player;
+
     }
+
 
     static int Calculate_player(int player) {
         System.out.println("current points of player = " + player);
@@ -38,6 +47,7 @@ public class SnakeAndLadder {
         System.out.println("Option is : " + option);
         if (option == 1) {
             int result = Calculate_ladder(dice_number, player);
+
             return result;
         } else if (option == 2) {
             int result = Calculate_snake(dice_number, player);
@@ -50,15 +60,23 @@ public class SnakeAndLadder {
     }
 
     public static void main(String[] args) {
-        while (PLAYER_POSITION < 100) {
-
-            PLAYER_POSITION = Calculate_player(PLAYER_POSITION);
-            System.out.println("total points of player : " + PLAYER_POSITION);
-            if (PLAYER_POSITION == 100) {
-                System.out.println("player has won the match");
-            }
+        while (PLAYER1_POSITION < 100 && PLAYER2_POSITION < 100) {
+            System.out.println("......................\nPlayer1 is playing now..\n.......................");
+            PLAYER1_POSITION = Calculate_player(PLAYER1_POSITION);
+            System.out.println("total points of player1 : " + PLAYER1_POSITION);
             count += 1;
-            System.out.println("total number of times dice was played : "+count);
+
+            System.out.println("......................\nPlayer2 is playing now..\n.......................");
+            PLAYER2_POSITION = Calculate_player(PLAYER2_POSITION);
+            System.out.println("total points of player2 : " + PLAYER2_POSITION);
+            count += 1;
+
         }
+        if (PLAYER1_POSITION == 100) {
+            System.out.println("Player1 won the game");
+        } else if (PLAYER2_POSITION == 100) {
+            System.out.println("player2 won the game");
+        }
+        System.out.println("total number of time dice was rolled : " + count);
     }
 }
